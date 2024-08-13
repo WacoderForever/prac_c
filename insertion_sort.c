ca216849229f3d5ed6ba9975adf397d0d61e944f
Wacoder Forever <sethomondi088@gmail.com>
2024-08-13 03:20:26 +0300

    ghjg

--- insertion_sort.c
+++ insertion_sort.c
@@ -2,19 +2,43 @@
 
 int main(){
 
-    int A[]={8,5,78,90,2,45,67};
-    int size=sizeof(A)/2;
-    int B[size];
-    B[0]=A[0];
-    int count=0;
-    while(count<size){
+    int a[]={8,5,78,90,2,45,67};
+    int size=sizeof(a)/4;
+    int b[size];
+    int count=1;
+    while(count<=size){
+        for(int i=0;i<size;i++){
+            if(a[count]<b[i]){
+                int c[count];
+                int k=i;
+                for(int j=0;j<count;j++){
+                    c[j]=b[k];
+                    k++;
+                }
+                b[i]=a[count];
+                k=0;
+                for(int j=i+1;j<=count;j++){
+                    b[j]=c[k];
+                    k++;
+                }
+            }
+            if(a[count]>=b[i]){
+              int c[count-i];
+              int k=i+1;
 
-        if(A[count+1]<B[count]){
-            int C[size-count];
-            for(int i=0;i<=count;i++){
-                C[i]=B[];
+              for(int j=0;j<count-i;j++){
+                c[j]=b[k];
+                k++;
+              }
+              b[i+1]=a[count];
+              k=0;
+              for(int j=i+2;j<size;j++){
+                b[j]=c[k];
+                k++;
+              }
             }
         }
         count++;
+        printf("%d %d %d %d %d %d %d\n",b[0],b[1],b[2],b[3],b[4],b[5],b[6]);
     }
 }
