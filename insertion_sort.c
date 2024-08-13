636c1b70d66ca9544f8ab4a87fe1e3e711a29f82
Wacoder Forever <sethomondi088@gmail.com>
2024-08-13 04:53:11 +0300

    hjg

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,41 +1,38 @@
 #include "allheaders.h"
 
-int main(){
+int main() {
+    long long int a[] = {27, 56, 12, 98, 45667, 6776};
+    int size = sizeof(a) / sizeof(a[0]);
+    long long int b[size];
+    b[0] = a[0];
+    int count = 1;
 
-    int a[]={27,56,12,98,45667,6776};
-    int size=sizeof(a)/4;
-    int b[size];
-    b[0]=a[0];
-    int count=1;
-
-    while(count<size){
-        if(a[count]<b[0]){
-            int c[count];
-            for(int i=0;i<count;i++){
-                c[i]=b[i];
+    while (count < size) {
+        if (a[count] < b[0]) {
+            long long int c[count];
+            for (int i = 0; i < count; i++) {
+                c[i] = b[i];
             }
-            b[0]=a[count];
-            for(int i=1;i<=count;i++){
-                b[i]=c[i-1];
+            b[0] = a[count];
+            for (int i = 1; i <= count; i++) {
+                b[i] = c[i - 1];
             }
-        }
-        else if(a[count]>=b[count-1]){
-            b[count]=a[count];
-        }
-        else{
-            for(int i=1;i<count;i++){
-                if(a[count]<b[i]){
-                    int csize=count-i;
-                    int k=i;
-                    int c[csize];
-                    for(int j=0;j<csize;j++){
-                        c[j]=b[k];
+        } else if (a[count] >= b[count - 1]) {
+            b[count] = a[count];
+        } else {
+            for (int i = 1; i < count; i++) {
+                if (a[count] < b[i]) {
+                    int csize = count - i;
+                    int k = i;
+                    long long int c[csize];
+                    for (int j = 0; j < csize; j++) {
+                        c[j] = b[k];
                         k++;
                     }
-                    b[i]=a[count];
-                    k=0;
-                    for(int j=i+1;j<count;j++){
-                        b[j]=c[k];
+                    b[i] = a[count];
+                    k = 0;
+                    for (int j = i + 1; j < count; j++) {
+                        b[j] = c[k];
                         k++;
                     }
                 }
@@ -44,8 +41,10 @@ int main(){
         count++;
     }
 
-    for(int i=0;i<size;i++){
-        printf("%d, ",b[i]);
+    for (int i = 0; i < size; i++) {
+        printf("%lld, ", b[i]);
     }
     printf("\n");
+
+    return 0;
 }
