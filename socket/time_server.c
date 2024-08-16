8f1bca5707c5af942af8c55d6ddc3e33536b1be9
Wacoder Forever <sethomondi088@gmail.com>
2024-08-17 01:41:07 +0300

    hjhgj

---
+++ socket/time_server.c
@@ -0,0 +1,19 @@
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib,"ws2_32.lib")
+
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif
+
