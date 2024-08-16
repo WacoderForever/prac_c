5fdea7a6cf972e176046799da633210929955291
Wacoder Forever <sethomondi088@gmail.com>
2024-08-17 02:18:49 +0300

    gfdfd

--- socket/time_server.c
+++ socket/time_server.c
@@ -17,3 +17,17 @@
 
 #endif
 
+//macro to abstract difference between Berkely and WinSock sockets
+
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+#else
+#define ISVALIDSOCKET(s) (s >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+#endif
+
