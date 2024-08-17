fbd6f63fa9e314cd60a16f8fb49c04132f663b8e
Wacoder Forever <sethomondi088@gmail.com>
2024-08-17 03:42:19 +0300

    hgnggfh

--- socket/time_server.c
+++ socket/time_server.c
@@ -57,4 +57,15 @@ int main(){
     struct addrinfo *bind_address;
     getaddrinfo(NULL,"8080",&hint,&bind_address);
 
+    //creating socket
+    printf("Creating socket.....\n");
+    SOCKET socket_listen;
+    socket_listen=socket(bind_address->ai_family,bind_address->ai_socktype,bind_address->ai_protocol);
+    if(!ISVALIDSOCKET(socket_listen)){
+        fprintf(stderr,"socket() failed. (%d)\n",GETSOCKETERRNO());
+        return 1;
+    }
+
+    //bind socket to the local address
+    
 }
