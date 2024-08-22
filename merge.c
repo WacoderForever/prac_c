8b28dc3583dbc85231c07b99dda4670ccaf28768
Wacoder Forever <sethomondi088@gmail.com>
2024-08-22 14:12:39 +0300

    hjtj

--- merge.c
+++ merge.c
@@ -5,16 +5,34 @@ void merge(int arr[],int left,int mid,int right){
     int n2=right-mid;
     int L[n1];
     int R[n2];
+
+    for(int i=0;i<n1;i++){
+        L[i]=arr[i];
+    }
+    for(int j=0;j<n2;i++){
+        R[i]=arr[j];
+    }
+
+    i=0;
+    j=0;
+    k=left;
+    while(i<n1 && j<n2){
+        if(L[i]<R[j]){
+            arr[k]=L[i];
+            i++;
+        }
+        else{
+            arr[k]=R[j];
+            j++
+        }
+        k++;
+    }
+}
+
+void MergeSort(int left,int right){
+    int mid=left+(right-left)/2;
     
 }
 int main(){
     int A[]={67,98,1000,54,-90,75};
-    int size=sizeof(A)/A[0];
-    int iter=(int)size/2;
-
-    while(iter<0){
-
-    }
-
-    return 0;
 }
