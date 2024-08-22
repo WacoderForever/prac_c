26025d9e9b79d4f7b22b47576b55ed65e44af751
Wacoder Forever <sethomondi088@gmail.com>
2024-08-22 14:39:04 +0300

    jkghjh

--- merge.c
+++ merge.c
@@ -1,38 +1,76 @@
 #include "allheaders.h"
 
-void merge(int arr[],int left,int mid,int right){
-    int n1=mid-left+1;
-    int n2=right-mid;
-    int L[n1];
-    int R[n2];
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;  
+    int n2 = right - mid;    
 
-    for(int i=0;i<n1;i++){
-        L[i]=arr[i];
-    }
-    for(int j=0;j<n2;i++){
-        R[i]=arr[j];
-    }
+    int L[n1], R[n2];
 
-    i=0;
-    j=0;
-    k=left;
-    while(i<n1 && j<n2){
-        if(L[i]<R[j]){
-            arr[k]=L[i];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    i = 0; 
+    j = 0; 
+    k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
             i++;
+        } else {
+            arr[k] = R[j];
+            j++;
         }
-        else{
-            arr[k]=R[j];
-            j++
-        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
         k++;
     }
 }
 
-void MergeSort(int left,int right){
-    int mid=left+(right-left)/2;
-    
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2; 
+
+        // Recursively sort the first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
 }
-int main(){
-    int A[]={67,98,1000,54,-90,75};
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+    return 0;
 }
