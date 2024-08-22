3da4d335fbcd7d63641df0e1241c570b896393c5
Wacoder Forever <sethomondi088@gmail.com>
2024-08-22 14:04:27 +0300

    jjujkgf

---
+++ merge.c
@@ -0,0 +1,20 @@
+#include "allheaders.h"
+
+void merge(int arr[],int left,int mid,int right){
+    int n1=mid-left+1;
+    int n2=right-mid;
+    int L[n1];
+    int R[n2];
+    
+}
+int main(){
+    int A[]={67,98,1000,54,-90,75};
+    int size=sizeof(A)/A[0];
+    int iter=(int)size/2;
+
+    while(iter<0){
+
+    }
+
+    return 0;
+}
