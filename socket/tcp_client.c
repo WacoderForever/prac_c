3f0231883d2ef1f17cbb4ae7f02ac6bff02f7c46
Wacoder Forever <sethomondi088@gmail.com>
2024-09-11 23:52:09 +0300

    fghgf

--- socket/tcp_client.c
+++ socket/tcp_client.c
@@ -1,34 +1,73 @@
-#if defined(_WIN32)
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0600
-#endif
-#pragma comment(lib,"ws2_32.lib")
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#else
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <errno.h>
-#include <unistd.h>
-#endif
+#include "chapter3.h"
 
 #if defined(_WIN32)
-#if !defined(IPV6_V6ONLY)
-#define IPV6_V6ONLY 27
-#endif
-#define CLOSESOCKET(s) (closesocket(s))
-#define GETSOCKETERRNO() (WSAGetLastError())
-#define ISVALIDSOCKET(s) (s != INVALID_SOCKET)
-#else
-#define SOCKET int
-#define GETSOCKETERRNO() (errno)
-#define CLOSESOCKET(s) (close(s))
-#define ISVALIDSOCKET(s) (s>=0)
+#include <conio.h>
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+int main(int argc,char **argv){
+    #if defined(_WIN32)
+        WSADATA d;
+        if(WSAStartup(MAKEWORD(2,2),&d)){
+            fprintf(stderr,"Failed to initialise.\n");
+            return 1;
+        }
+    #endif
+    if(argc < 3){
+        fprintf(stderr,"usage: tcp_client hostname port.\n");
+        return 1;
+    }
+
+    printf("Configuring remote address....\n")
+    struct addrinfo hints;
+    memset(&hints,0,sizeof(hints));
+    hints.ai_socktype=SOCK_STREAM;
+    struct sockaddr *peer_address;
+    if(getaddrinfo(argv[1],argv[2],&hints,&peer_address)){
+        fprintf(stderr,"getaddrinfo() failed. (%d)",GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Remote address is:\n");
+    char address_buffer[1000];
+    char service_buffer[1000];
+    getnameinfo(peer_address->ai_addr,peer_address->ai_addrlen,
+                address_buffer,sizeof(address_buffer),
+                service_buffer,sizeof(service_buffer),NI_NUMERICHOST);
+    printf("%s %s\n",address_buffer,service_buffer);
+
+    printf("Creating socket.....\n");
+    SOCKET socket_peer;
+    socket_peer=socket(peer_address->ai_family,peer_address->ai_socktype,peer_address->ai_protocol);
+    if(!ISVALIDSOCKET(socket_peer)){
+        fprintf(stderr,"socket() failed.(%d)\n",GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Connecting to the remote server...\n");
+    if(connect(socket_peer,peer_address->ai_addr,peer_address->ai_addrlen)){
+        fprintf(stderr,"connect() failed. (%d)",GETSOCKETERRNO());
+        return 1;
+    }
+    freeaddrinfo(peer_address);
+    printf("Connected.\n");
+    printf("To send data enter text followed by enter.\n");
+
+    while(1){
+
+        fd_set reads;
+        FD_ZERO(reads);
+        FD_SET(socket_peer,&reads);     
+        #if defined(_WIN32)
+            FD_SET(0,&reads);
+        #endif
+        struct timeval timeout;
+        timeout.tv_sec=0;
+        timeout.tv_usec=100000;
+        if((select(socket_peer+1,&reads,0,0,&timeout))<0){
+            fprintf(stderr,"select() failed. (%d)\n",GETSOCKETERRNO());
+            return 1;
+        }
+        
+
+    }
+}
