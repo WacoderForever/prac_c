c03baf670f8e972da00caa048123dbaac9eed66d
Wacoder Forever <sethomondi088@gmail.com>
2025-01-08 01:49:40 +0300

    dfgfd

--- compiler/Tiger/chap_1/main.c
+++ compiler/Tiger/chap_1/main.c
@@ -8,7 +8,33 @@
  * arguments of any print statement within any subexpression of a given
  * statement. For example,maxargs(prog) is 2.
  */
+int maxargs(A_stm);
 
- int maxargs_exp(){
-    
- }
+int maxargs_exp(A_exp e) {
+  switch (e->kind) {
+    case A_idExp:  return 0;
+    case A_numExp: return 0;
+    case A_opExp:
+      return maxargs_exp(e->u.op.left) + maxargs_exp(e->u.op.right);
+    case A_eseqExp:
+      return maxargs(e->u.eseq.stm) + maxargs_exp(e->u.eseq.exp);
+  }
+}
+
+int maxargs_exp_list(A_expList el) {
+  if (el->kind == A_lastExpList)
+    return maxargs_exp(el->u.last);
+  else
+    return maxargs_exp(el->u.pair.head) + maxargs_exp_list(el->u.pair.tail);
+}
+
+int maxargs(A_stm s) {
+  switch (s->kind) {
+    case A_compoundStm:
+      return maxargs(s->u.compound.stm1) + maxargs(s->u.compound.stm2);
+    case A_assignStm:
+      return maxargs_exp(s->u.assign.exp);
+    case A_printStm:
+      return 1 + maxargs_exp_list(s->u.print.exps);
+  }
+}
