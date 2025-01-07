d8578e7efe125153d3795ff0e70a26c4b777e78c
Wacoder Forever <sethomondi088@gmail.com>
2025-01-07 04:13:12 +0300

    hhgjgh

---
+++ compiler/Tiger/chap_1/main.c
@@ -0,0 +1,14 @@
+#include "util.h"
+#include "slp.h"
+#include "prog1.h"
+#include <stdio.h>
+
+/**
+ * 1. Write a function int maxargs(A_stm) that tells the maximum number of
+ * arguments of any print statement within any subexpression of a given
+ * statement. For example,maxargs(prog) is 2.
+ */
+
+ int maxargs_exp(){
+    
+ }
