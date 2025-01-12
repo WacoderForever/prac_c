b50f4648856b907b8463ac016dac928c717b8687
Wacoder Forever <sethomondi088@gmail.com>
2025-01-12 04:40:29 +0300

    gvngh

--- compiler/Tiger/chap_1/main.c
+++ compiler/Tiger/chap_1/main.c
@@ -38,3 +38,101 @@ int maxargs(A_stm s) {
       return 1 + maxargs_exp_list(s->u.print.exps);
   }
 }
+
+/**
+ * 2. Write a function void interp(A_stm) that “interprets” a programin this
+ * language.To write in a “functionalprogramming”style – in which younever use
+ * an assignment statement– initialize each local variable as you declare it.
+ */
+
+//definition
+void interp(A_stm s);
+
+typedef struct table* Table_;
+struct table{string id;int value;Table_ tail;};
+Table_ Table(string id,int value,struct table* tail){
+  Table_ t=malloc(sizeof(*t));
+  t->id=id;
+  t->value=value;
+  t->tail=tail;
+  return t;
+}
+
+Table_ interpStm(A_stm s,Table_ t);
+Table_ update(Table_ t,string id,int value);
+int lookup(Table_ t,string id);
+
+struct IntAndTable{int i;Table_ t;};
+struct IntAndTable IntAndTable(int i,Table_ t){
+  struct IntAndTable it=malloc(sizeof(it));
+  it.i=i;
+  it.t=t;
+  return it;
+}
+struct IntAndTable interpExp(A_exp e,Table_ t);
+//implementation
+void interp(A_stm s){
+  Table_ t=NULL;
+  interpStm(s,t);
+}
+
+Table_ interPrintExpList(A_expList el,Table_ t){
+  if(el->kind==A_lastExpList){
+    struct IntAndTable it=interExp(el->u.last,t);
+    printf("%d\n",it.i);
+    return it.t
+  }
+  else{
+    struct IntAndTable it=interExp(el->u.pair.head,t);
+    printf("%d\n",it.i);
+    return interPrintExpList(el->u.pair.tail,it.t);
+  }
+}
+
+Table_ interpStm(A_stm s,Table_ t){
+  switch(s->kind){
+    case A_compoundStm:
+      return interpStm(s->u.compound.stm1,interpStm(s->u.compound.stm2,t));
+    case A_assignStm:
+      return update(t,s->u.assign.id,interExp(s->u.assign.exp,t).i);
+    case A_printStm:
+      return interPrintExpList(s->u.print.exps,t);
+  }
+}
+
+struct IntAndTable interExp(A_exp e,Table_ t){
+  switch(e->kind){
+    case A_numExp:
+      return IntAndTable(e->u.num,t);
+    case A_idExp:
+      return IntAndTable(lookup(t,e->u.id),t);
+    case A_opExp:{
+      struct IntAndTable left=interExp(e->u.op.left,t);
+      struct IntAndTable right=interExp(e->u.op.right,left.t);
+      switch(e->u.op.oper){
+        case A_plus:
+          return IntAndTable(left.i + right.i,right.t);
+        case A_minus:
+          return IntAndTable(left.i - right.i,right.t);
+        case A_times:
+          return IntAndTable(left.i * right.i,right.t);
+        case A_div:
+          return IntAndTable(left.i / right.i,right.t);
+      }
+    }
+    case A_eseqExp:
+      return interExp(interExp(e->u.eseq.exp,t),interpStm(e->u.eseq.stm,t));
+  }
+}
+
+Table_ update(Table_ t,string id,int value){
+  return Table(id,value,t);
+}
+
+int lookup(Table_ t,string id){
+  if(t==NULL) return -1;
+  else{
+    if(t->id==id) return t->value;
+    return lookup(t->tail,id);
+  }
+}
