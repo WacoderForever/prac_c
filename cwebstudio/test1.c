eb021d9a202614881ad386e6efdc08399bc6eb03
WacoderForever <sethomondi088@gmail.com>
2025-03-02 11:03:30 +0300

    cweb_test

---
+++ cwebstudio/test1.c
@@ -0,0 +1,16 @@
+#include "CWebStudio.h"
+
+CwebNamespace cweb;
+
+CwebHttpResponse *main_sever(CwebHttpRequest *request ){
+
+    return cweb.response.send_text("Hello World", 200);
+
+}
+
+int main(int argc, char *argv[]){
+    cweb = newCwebNamespace();
+    struct CwebServer server = newCwebSever(5000, main_sever);
+    cweb.server.start(&server);
+    return 0;
+}
