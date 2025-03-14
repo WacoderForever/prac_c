f2801afa02b5eb9974a255eb3904556567914eb2
WacoderForever <sethomondi088@gmail.com>
2025-03-14 15:58:37 +0300

    merge2_good

--- trials/merge2.c
+++ trials/merge2.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void merge_sort(int Arr[],int left,int right){
-    if (right <= left ) return ;
-    int mid = left + (right - left)/2;
-    merge_sort(Arr,left,mid);
-    merge_sort(Arr,mid+1,right);
-    int A[mid-left+1];
-    int B[right-mid];
-    for(int i=0;i<=mid-left;i++){
-        A[i]=Arr[i];
+void merge(int arr[],int left,int mid,int right){
+    int n1=mid-left+1;
+    int n2=right-mid;
+    int A[n1];
+    int B[n2];
+
+    for(int i=0;i<n1;i++) A[i] = arr[left+i];
+    for(int j=0;j<n2;j++) B[j] = arr[mid+1+j];
+
+    int i=0;
+    int j=0;
+    int k=left;
+    while(i<n1 && j<n2){
+        if(B[j]<A[i]) arr[k++] = B[j++];
+        else arr[k++]=A[i++];
     }
-    int k=0;
-    for(int i=mid-left+1;i<=right;i++){
-        B[k]=Arr[i];
-        k++;
-    }
-    int l=0;
-    int n=0;
-    for(int i=left;i<=right;i++){
-        if(B[l] < A[n]) Arr[i] = B[l++];
-        else Arr[i] = A[n++];
-    }
+
+    //copy remaining elements
+    while(i<n1) arr[k++] = A[i++];
+    while(j<n2) arr[k++] = B[j++];
 }
 
-void printArray(int *A, int size) {
+void merge_sort(int arr[],int left,int right){
+    if(right <= left) return;
+    int mid = left +(right - left)/2;
+    merge_sort(arr,left,mid);
+    merge_sort(arr,mid+1,right);
+    merge(arr,left,mid,right);
+}
+
+void printArray(int A[], int size) {
     for (int i = 0; i < size; i++) {
         printf("%d ", A[i]);
     }
@@ -32,12 +39,12 @@ void printArray(int *A, int size) {
 }
 
 int main() {
-    int A[] = {5, 6, 12, 7, -8, 790};
+    int A[] = {789,905,-98,056};
     int size = sizeof(A) / sizeof(A[0]); 
     printf("Initial array:\n");
     printArray(A, size);
     merge_sort(A,0,size-1);
-    printf("Final array:\n");
+    printf("Final sorted array:\n");
     printArray(A, size);
     return 0;
 }
