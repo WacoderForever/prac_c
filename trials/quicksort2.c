d732d68e226d01efdc341d405d80f35726a49fd2
WacoderForever <sethomondi088@gmail.com>
2025-04-01 02:02:53 +0300

    fghgdfhfg

--- trials/quicksort2.c
+++ trials/quicksort2.c
@@ -7,26 +7,6 @@ void swap(int *a,int *b){
     *a=temp;
 }
 
-int partition(int arr[],int low,int high){
-    int pivot=arr[low];
-    int k=low;
-    for(int i=low+1;i<=high;i++){
-        if(arr[i]<arr[k]){
-            swap(&arr[i],&arr[k]);
-            k++;
-        }
-    }
-    return k;
-}
-
-void quickSort(int arr[],int low,int high){
-    if(low<=high){
-        int pi=partition(arr,low,high);
-        quickSort(arr,low,pi);
-        quickSort(arr,pi+1,high);
-    }
-}
-
 void printArray(int A[],int size){
     for(int i=0;i<size;i++){
         printf("%d ",A[i]);
