e2541fb591db4cc1c4d695298d41e6203ae41cb6
WacoderForever <sethomondi088@gmail.com>
2025-12-21 13:01:19 +0300

    huff_man1

---
+++ trials/huffman_coding.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct PQueue{
+    char c;
+    int occurences;
+    bool is_char;
+    bool is_num;
+    PQueue *next;
+    bool is_last;
+
+}PQueue;
+
+PQueue create_PQ_item(){
+    
+}
+
+void insert_PQ_item(PQueue **queue){
+
+}
+
+int main(){
+
+}
