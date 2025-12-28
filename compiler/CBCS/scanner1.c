0970990fa5df98ccbc329ea902fbd74b64edad4f
WacoderForever <sethomondi088@gmail.com>
2025-12-29 00:22:07 +0300

    scanner_still

--- compiler/CBCS/scanner1.c
+++ compiler/CBCS/scanner1.c
@@ -2,6 +2,15 @@
 #include <ctype.h>
 #include <string.h>
 
+int isspecial(char d,char *special_list){
+    for(int i=0;i<9;i++){
+        if(d==special_list[i]){
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main(){
     FILE *input,*output;
     int l=0;
@@ -12,30 +21,30 @@ int main(){
     input = fopen("input.txt","r");
     output = fopen("output.txt","w");
     char keyword[30][30] = {"int","main","if","else","do","while"};
-    char special[30] = {';','{','}','(',')','?','@','!','%'};
+    char special[9] = {';','{','}','(',')','?','@','!','%'};
     fprintf(output,"Line no.\t Token no.\t Token\t Lexeme\n\n");
     while(!feof(input)){
         i=0;
         flag=0;
         ch = fgetc(input);
         if((ch == '+')||(ch == '-')||(ch == '/')||(ch =='*')){
-            fprintf(output,"%7d\t%7d\t Operator %7c\n",l,t,ch);
+            fprintf(output,"%7d\t%7d\t Operator\t %7c\n",l,t,ch);
             t++;
         }
         else if( ch==';' || ch=='{' || ch=='}' || ch=='(' || ch==')' || ch=='?' ||
                 ch=='@' || ch=='!' ||ch=='%'){
-            fprintf(output,"%7d\t%7d\t Special Symbol %7c\n",l,t,ch);
+            fprintf(output,"%7d\t%7d\t Special Symbol\t %7c\n",l,t,ch);
             t++;
         }
         else if(isdigit(ch)){
-           fprintf(output,"%7d\t%7d\t Digit %7c\n",l,t,ch);
+           fprintf(output,"%7d\t%7d\t Digit\t %7c\n",l,t,ch);
            t++; 
         }
         else if(isalpha(ch)){
             str[i] = ch;
             i++;
             ch = fgetc(input);
-            while(isalpha(ch) && ch != ' '){
+            while(isalpha(ch) && ch != ' ' && !(isspecial(ch,special))){
                 str[i] = ch;
                 i++;
                 ch = fgetc(input);
@@ -48,11 +57,11 @@ int main(){
                 }
             }
             if(flag){
-                fprintf(output,"%7d\t%7d\t Keyword %7s\n",l,t,str);
+                fprintf(output,"%7d\t%7d\t Keyword\t %7s\n",l,t,str);
                 t++;
             }
             else{
-                fprintf(output,"%7d\t%7d\t Identifier %7s\n",l,t,str);
+                fprintf(output,"%7d\t%7d\t Identifier\t %7s\n",l,t,str);
                 t++;
             }
         }
