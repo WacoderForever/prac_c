80c1abbf811f061fa0604669e3b98606c7029aab
WacoderForever <sethomondi088@gmail.com>
2026-01-12 15:11:15 +0300

    time

--- trials/time.c
+++ trials/time.c
@@ -4,6 +4,6 @@
 int main(){
     time_t largest=0x7fffffff;
     printf("Biggest=%s\n",asctime(gmtime(&largest)));
-    printf("Size of time_t: %u",sizeof(time_t));
+    printf("Size of time_t: %u\n",sizeof(time_t));
     return 0;
 }
